c28e5619fe52c7b45575237f384a92d92d37e02a
t-VERONICA WAKILEH <t-veronica.wakileh@exalt.ps>
2026-08-26 10:54:57 +0300

    final

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,18 +9,18 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.sql.SQLException;
 
 // Driver Class
-public class Main{
+public class Main {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         try {
             HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
 
             server.createContext("/", new MyHandler());
 
-            server.setExecutor(null); 
+            server.setExecutor(null);
             server.start();
 
             System.out.println("Server is running on port 8000");
@@ -32,29 +32,42 @@ public class Main{
     // Define a custom HttpHandler
     static class MyHandler implements HttpHandler {
         @Override
-        public void handle(HttpExchange exchange) throws IOException
-        {
+        public void handle(HttpExchange exchange) throws IOException {
+
             Gson gson = new Gson();
             CoursesService coursesService = new CoursesService();
+
             String requestMethod = exchange.getRequestMethod();
+
             String path = exchange.getRequestURI().getPath();
-            String role = exchange.getRequestHeaders().getFirst("ROLE");
+            String role = exchange.getRequestHeaders().getFirst("Authorization");
             String[] subDirs = path.split("/");
+            //System.out.println(exchange.getRequestHeaders().get("Authorization"));
 
-            if(requestMethod.equals("GET")){
-                if (subDirs.length == 3 && path.equals("/api/v1/Courses")){
+            if (requestMethod.equals("GET") && subDirs[3].equals("/Courses")) {
+                if (subDirs.length == 4) {
                     //1
-                }
-                else if (subDirs.length > 3){
-                    if (role.equals("USER")){
+                } else if (subDirs.length > 4) {
+                    if (role.equals("USER")) {
+                        Long id = Long.parseLong(subDirs[4]);
+                        String responseGet = null;
+                        try {
+                            responseGet = gson.toJson(coursesService.GetCourse(id));
+                        } catch (SQLException e) {
+                            throw new RuntimeException(e);
+                        }
+                        exchange.sendResponseHeaders(200, responseGet.length());
+                        OutputStream os = exchange.getResponseBody();
+                        os.write(responseGet.getBytes());
+                        os.close();
 
-                    } else // 403
+                    } else {
+                    }
 
-                }
-                else {
+                } else {
                     // 404 not found
                 }
-                
+              /*
             } else if (requestMethod.equals("POST")) {
                 if (role.equals("ADMIN")){
 
@@ -69,14 +82,16 @@ public class Main{
                 if (role.equals("ADMIN")){
 
                 } else // 403
-            }
+            }*/
 
-            // Handle the request
-            String response = "Hello, this is a simple HTTP server response!";
-            exchange.sendResponseHeaders(200, response.length());
-            OutputStream os = exchange.getResponseBody();
-            os.write(response.getBytes());
-            os.close();
+                // Handle the request
+                String response = "Hello, this is a simple HTTP server response!";
+
+                //exchange.sendResponseHeaders(200, response.length());
+                //OutputStream os = exchange.getResponseBody();
+                //os.write(response.getBytes());
+                //os.close();
+            }
         }
     }
 }
